a6be45fd3b3d5fbd532712b3b26b0e2fce1ef5cf
howard <howardnlee94@gmail.com>
2022-12-22 15:13:37 -0800

    condensed js code and add readme/screenshots

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Password
+Password Generator Project
+
+## Description
+
+This project is to create a password generator. It includes an input that ask user for a number and confirm boxes for yes and no options. 
+
+This project demonstrate the knowledge of using for loops, conditional statements, and arrays.
+
+## Usage
+When user press on generate password it will prompt user to input a number
+Then there will be additional questions to hit confirm or cancel on.
+Once done a randomly generate password will be displayed on screen.
+
+## Features
+Generates password 
+
--- script.js
+++ script.js
@@ -22,13 +22,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// created a function that ask for a number
 function askNum(){
-    var userchoice = prompt("choose a number between 10-500");
-      {if (userchoice > 500){
+    var userchoice = prompt("choose a number between 8-128");
+      {if (userchoice > 128){
         window.alert("Too big, Try again");
         askNum();
-      }else if(userchoice<10){
+      }else if(userchoice<8){
         window.alert("Too Small, Try Again");
         askNum();
       }else {
@@ -36,14 +36,14 @@ function askNum(){
         }
       }
   }
-
+//created a function that checks if atleast one option was chosen
 function typeChar(){
     if(!speChar && !numChar && !lowChar && !upChar){
       window.alert("Need to Choose one. Try Again");
       generatePassword();
     }
   }
-
+//generates a password
 function createPass(){
       for(i=0; i<=numPass; i++){
         x=Math.floor(Math.random() * paSelection.length)
@@ -52,92 +52,38 @@ function createPass(){
       }
   }
 
+  //This runs the required functions to generate the password with all condition when the button is pressed.
 function generatePassword(){
+  paSelection=[];
   password="";
-  askNum();
-  speChar = confirm("Do you want special character?");
-  numChar = confirm("Do you want numbers?");
-  lowChar = confirm("Do you want lower case letters?");
-  upChar = confirm("Do you want Upper Case Letters?");
-  
-  typeChar();
-  
   var NuminPass = ["0","1","2","3","4","5","6","7","8","9"];
   var lowCharinPass = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upCharinPass=  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var specinPass = ["!","@","#","$","%","^","&","*","(",")","_","+"];
-
-
-
-  if((!speChar&&numChar)&&(lowChar&&upChar)){
-
-    paSelection = [lowCharinPass, upCharinPass, NuminPass];
-
-  }else if((!speChar&&!numChar)&&(lowChar&&upChar)){
-
-    paSelection = [lowCharinPass, upCharinPass,];
-
-  }else if((!speChar&&!numChar)&&(!lowChar&&upChar)){
-
-    paSelection = [upCharinPass];
-  }else if ((!speChar&&numChar)&&(!lowChar&&upChar)){
-    
-    paSelection = [NuminPass,upCharinPass];
-
-   }else if ((speChar&&!numChar)&&(lowChar&&upChar)){
-
-     paSelection = [lowCharinPass, upCharinPass, specinPass];
-  
-  }else if ((speChar&&!numChar)&&(!lowChar&&upChar)){
-
-    paSelection = [upCharinPass, specinPass];
-
-  }else if ((speChar&&!numChar)&&(!lowChar&&!upChar)){
-
-    paSelection = [specinPass];
-
-  }else if ((speChar&&!numChar)&&(lowChar&&!upChar)){
-
-    paSelection =[specinPass, lowCharinPass];
-
-  }else if ((speChar&&numChar)&&(!lowChar&&upChar)){
-
-    paSelection = [NuminPass, upCharinPass, specinPass];
-
-  }else if ((speChar&&numChar)&&(!lowChar&&!upChar)){
-    
-    paSelection = [NuminPass, specinPass];
-
-  }else if ((!speChar&&numChar)&&(!lowChar&&!upChar)){
-
-    paSelection = [NuminPass];
-
-  }else if ((!speChar&&numChar)&&(!lowChar&&upChar)){
-    
-    paSelection = [NuminPass, upCharinPass];
-
-  }else if ((speChar&&numChar)&&(lowChar&&!upChar)){
-
-    paSelection = [NuminPass,specinPass,lowCharinPass];
-
-  }else if ((!speChar&&numChar)&&(lowChar&&!upChar)){
-
-    paSelection = [NuminPass, lowCharinPass];
-
-  }else if ((!speChar&&!numChar)&&(lowChar&&!upChar)){
-
-    paSelection = [lowCharinPass];
-
-  }else if ((speChar&&!numChar)&&(lowChar&&!upChar)){
-
-    paSelection =[ lowCharinPass, specinPass];
-
-  }else{
-    paSelection= [NuminPass, specinPass, upCharinPass, lowCharinPass];
-  }
-  
+//ask for number of character in password
+  askNum();
+//ask for type of character in password
+  speChar = confirm("Do you want special character?");
+    if(speChar){
+      paSelection.push(specinPass);
+    }
+  numChar = confirm("Do you want numbers?");
+    if(numChar){
+      paSelection.push(NuminPass);
+    }
+  lowChar = confirm("Do you want lower case letters?");
+    if(lowChar){
+      paSelection.push(lowCharinPass);
+    }
+  upChar = confirm("Do you want Upper Case Letters?");
+    if(upChar){
+      paSelection.push(upCharinPass);
+    }
+// Checks if atleast one option was chosen 
+  typeChar();
+//generates the password
   createPass();
-
+//returns the password on screen
   return password;
 }
 
@@ -170,96 +116,6 @@ function generatePassword(){
 
 
 
-//---------------------------------------------------------------------------------------------------------
-
-
-
-
-//to make sure user chooses atleast one option
-
-// askNum();
-
-
-// //make user select option
-// var speChar = confirm("Do you want special character?");
-// var numChar = confirm("Do you want numbers?");
-// var lowChar = confirm("Do you want lower case letters?");
-// var upChar = confirm("Do you want Upper Case Letters?");
-
-// typeChar();
-
-// var NuminPass = ["0","1","2","3","4","5","6","7","8","9"];
-// var lowCharinPass = ["a","b","c","d","e","f,","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var upCharinPass=  ["A","B","C","D","E","F,","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var specinPass = ["!","@","#","$","%","^","&","*","(",")","_","+"];
-
-//   if((!speChar&&numChar)&&(lowChar&&upChar)){
-
-//     var paSelection = [lowCharinPass, upCharinPass, NuminPass];
-
-//   }else if((!speChar&&!numChar)&&(lowChar&&upChar)){
-
-//     var paSelection = [lowCharinPass, upCharinPass,];
-
-//   }else if((!speChar&&!numChar)&&(!lowChar&&upChar)){
-
-//     var paSelection = [upCharinPass];
-
-//   }else if ((speChar&&!numChar)&&(lowChar&&upChar)){
-
-//     var paSelection = [lowCharinPass, upCharinPass, specinPass];
-  
-//   }else if ((speChar&&!numChar)&&(!lowChar&&upChar)){
-
-//     var paSelection = [upCharinPass, specinPass];
-
-//   }else if ((speChar&&!numChar)&&(!lowChar&&!upChar)){
-
-//     var paSelection = [specinPass];
-
-//   }else if ((speChar&&numChar)&&(!lowChar&&upChar)){
-
-//     var paSelection = [NuminPass, upCharinPass, specinPass];
-
-//   }else if ((speChar&&numChar)&&(!lowChar&&!upChar)){
-    
-//     var paSelection = [NuminPass, specinPass];
-
-//   }else if ((!speChar&&numChar)&&(!lowChar&&!upChar)){
-
-//     var paSelection = [NuminPass];
-
-//   }else{
-//     var paSelection= [NuminPass, specinPass, upCharinPass, lowCharinPass];
-//   }
-
-
-// password = 
-
-
-
-
-// }
-// createPass();
-
-
-
-// console.log(password);
-// console.log(paSelection);
-// console.log(numPass);
-
-
-// var array1 = [0,1,2,3,4,5,6,7,8,9];
-// var array2 = ["a","b","c","d","e","f,","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var array3=  ["A","B","C","D","E","F,","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var array4 = ["!","@","#","$","%","^","&","*","(",")","_","+"];
-
-// var array5=[array1,array2,array3];
-// i=Math.floor(Math.random() * array5.length);
-// y=array5[i][Math.floor(Math.random() * array5[i].length)];
-// console.log(i);
-// console.log(y);
-// console.log(array5[1][2]);
 
 
 
